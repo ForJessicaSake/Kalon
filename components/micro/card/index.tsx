6b26266ead18b05a687c37b1c1c56621a6be0ce6
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-09-15 12:54:28 +0100

    fix

--- components/micro/card/index.tsx
+++ components/micro/card/index.tsx
@@ -5,6 +5,7 @@ import Button from "../button/Button";
 
 const Card = () => {
   const [products, setProducts] = React.useState<null | any>(null);
+
   React.useEffect(() => {
     const fetchProducts = async () => {
       const { data, error } = await Supabase.from("crotchet").select();
@@ -15,16 +16,17 @@ const Card = () => {
     };
     fetchProducts();
   });
+
   return (
     <div className="grid grid-cols-3 items-center place-items-center gap-10">
       {products &&
-        products.map((product: { [key: string]: string }) => (
-          <div className="p-5 font-medium flex flex-col items-center" key={product.id}>
+        products?.map((product: { [key: string]: string }) => (
+          <div className="p-5 font-medium flex flex-col items-center" key={product?.id}>
             <div>
-              <img src={product.image} alt="dress" className="w-64 h-64" />
+              <img src={product?.image} alt="dress" className="w-64 h-64" />
             </div>
-            <p className="py-3">{product.name}</p>
-            <p>${product.price}.00</p>
+            <p className="py-3">{product?.name}</p>
+            <p>${product?.price}.00</p>
             <Button className="my-3">Add To Cart</Button>
           </div>
         ))}
